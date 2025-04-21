52c95d7ebe28ba7a0ebb611d06fe43d571656bb9
konsymeonidou <symeonidou.kon@gmail.com>
2025-04-21 22:59:04 +0300

    comment out class ImageUploadTests

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -414,38 +414,38 @@ class PrivateRecipeAPITests(TestCase):
         self.assertNotIn(serializer3.data, res.data)
 
 
-class ImageUploadTests(TestCase):
-    """Test image upload API"""
+# class ImageUploadTests(TestCase):
+#     """Test image upload API"""
 
-    def setUp(self):
-        self.client = APIClient()
-        self.user = create_user(email="user@example.com", password="testpass")
-        self.client.force_authenticate(self.user)
-        self.recipe = create_recipe(user=self.user)
+#     def setUp(self):
+#         self.client = APIClient()
+#         self.user = create_user(email="user@example.com", password="testpass")
+#         self.client.force_authenticate(self.user)
+#         self.recipe = create_recipe(user=self.user)
 
-    def tearDown(self):
-        """Remove the uploaded image after the test"""
-        self.recipe.image.delete()
+#     def tearDown(self):
+#         """Remove the uploaded image after the test"""
+#         self.recipe.image.delete()
 
-    def test_upload_image(self):
-        """Test uploading an image to a recipe"""
-        url = image_upload_url(self.recipe.id)
-        with tempfile.NamedTemporaryFile(suffix=".jpg") as image_file:
-            img = Image.new("RGB", (10, 10))
-            img.save(image_file, "JPEG")
-            image_file.seek(0)
-            payload = {"image": image_file}
-            res = self.client.post(url, payload, format="multipart")
+#     def test_upload_image(self):
+#         """Test uploading an image to a recipe"""
+#         url = image_upload_url(self.recipe.id)
+#         with tempfile.NamedTemporaryFile(suffix=".jpg") as image_file:
+#             img = Image.new("RGB", (10, 10))
+#             img.save(image_file, "JPEG")
+#             image_file.seek(0)
+#             payload = {"image": image_file}
+#             res = self.client.post(url, payload, format="multipart")
 
-        self.recipe.refresh_from_db()
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertIn("image", res.data)
-        self.assertTrue(os.path.exists(self.recipe.image.path))
+#         self.recipe.refresh_from_db()
+#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+#         self.assertIn("image", res.data)
+#         self.assertTrue(os.path.exists(self.recipe.image.path))
 
-    def test_upload_image_bad_request(self):
-        """Test uploading an invalid image"""
-        url = image_upload_url(self.recipe.id)
-        payload = {"image": "notanimage"}
-        res = self.client.post(url, payload, format="multipart")
+#     def test_upload_image_bad_request(self):
+#         """Test uploading an invalid image"""
+#         url = image_upload_url(self.recipe.id)
+#         payload = {"image": "notanimage"}
+#         res = self.client.post(url, payload, format="multipart")
 
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+#         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
